fcb0322d437b5d0d041ce9f556008c53babd43ad
vignesh s <vigneshvicky6105@gmail.com>
2026-02-17 18:12:27 +0530

    comment uupdate to check git

--- app.py
+++ app.py
@@ -121,3 +121,4 @@ if id_file and live_image:
                 os.remove(id_path)
             if os.path.exists(live_path):
                 os.remove(live_path)
+#thsi comment is been given just to check the update of the git hub thing
